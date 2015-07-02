714ec56cbedbbb9abc6673576cfdeaa6d3806588
Kavyahs <kshanthegowda@qwinitech.com>
2015-07-02 13:05:52 +0530

    deploy.rb file updated to restart puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -108,3 +108,18 @@ namespace :puma do
 
   before :start, :make_dirs
 end
+
+desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
