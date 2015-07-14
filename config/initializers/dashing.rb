eb572abacde349c11a95f4dee515cd8501679f36
KJsharmila <sharmila437@gmail.com>
2015-07-14 18:14:36 +0530

    changed timeout

--- config/initializers/dashing.rb
+++ config/initializers/dashing.rb
@@ -8,7 +8,8 @@ Dashing.configure do |config|
   config.redis_host     = '127.0.0.1'
   config.redis_port     = '6379'
   config.redis_password = nil
-  # config.redis_timeout  = 3
+  config.redis_concurrency = 25
+  config.redis_timeout  = 30
 
   # Redis namespace when pushing new data.
   config.redis_namespace = 'dashing_events'
